07a1464d7817b69865fb110f68d90216cf9a1452
Andrzej Mazur <andrzej.mazur@end3r.com>
2011-08-05 10:58:20 +0200

    fixed movement of the stars

--- js/onspacestart.js
+++ js/onspacestart.js
@@ -18,9 +18,16 @@ GAME.Init = function() {
 	player.width = 98;
 	player.height = 109;
 	player.position((background.width-player.width)/2, 280, 1).speed(0);
-	
-	var items = GAME.generateItems(5, player);
+	// tmp:
+	player.size(69,75);
 
+	var itemCount = 5, items = [];
+	for(var i = 0; i < itemCount; i++) {
+		items[i] = new Mibbu.spr('img/star.png', 25, 25, 1, 0);
+		items[i].position(Math.random()*800, Math.random()*400, 0).speed(0);
+		items[i].hit(player, function() { GAME.gameOver(); });
+	}
+	
 	Mibbu.on();
 
 	var gameLoop = function(){
@@ -53,27 +60,26 @@ GAME.Init = function() {
 		else if(actSpeed < 0) {
 			actHeight -= 0.2;
 		}
+
+		for(var i = 0; i < itemCount; i++) {
+			items[i].position(items[i].position().x, items[i].position().y += actSpeed);
+			if(items[i].position().y > 400) {
+				newItem = new Mibbu.spr('img/star.png', 25, 25, 1, 0);
+				newItem.position(Math.random()*800, 0, 0).speed(0);
+				newItem.hit(player, function() { GAME.gameOver(); });
+				items.splice(i,1,newItem);
+			}
+		}
 		
-		// TODO: for every item (star) add Y to stars' positions so it will be moving like background
-			
+		// TODO: change stars to something else, like balloons etc, add (generate) flying birds from given height
+	
 		background.speed(actSpeed);
 		GAME.Config.data.height = actHeight;
 	}
 
 	Mibbu.hook(gameLoop).hitsOn();
 	
-	player.zone(10,10,10,10);
+	player.zone(30,10,30,10);
 }
 
-GAME.generateItems = function(count, player) {
-	var items = [];
-	for(var i = 0; i < count; i++) {
-		items[i] = new Mibbu.spr('img/star.png', 25, 25, 1, 0);
-		items[i].position(Math.random()*800, Math.random()*400, 0).speed(0);
-		items[i].hit(player, function(){
-			console.log('hit!');
-			/* TODO: remove the star, add points, generate another one at the top of the page */
-		});
-	}
-	return items;
-};
+GAME.gameOver = function(id) { alert('GAME OVER! ;('); }
